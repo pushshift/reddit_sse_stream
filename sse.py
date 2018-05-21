7cd8233a0cf3a3a7322b346665785b9e8c52e91d
root <jason@pushshift.io>
2018-05-21 08:06:34 -0400

    Change port to 5000

--- sse.py
+++ sse.py
@@ -211,4 +211,4 @@ def stream():
     return Response(eventStream(), mimetype="text/event-stream")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=9000)
+    app.run(host='0.0.0.0',port=5000)
